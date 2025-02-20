b69668dfe7978a8529fa2eb3fffca3189ec86629
Borsch <l.boristag@gmail.com>
2025-02-21 02:13:43 +0300

    fixed GET

--- response.py
+++ response.py
@@ -11,5 +11,5 @@ def make_response(data):
 def send_response(handler, status_code, response_data):
     handler.send_response(status_code)
     handler.send_header("Content-type", "application/json")
-    handler.send_headers()
+    handler.end_headers()
     handler.wfile.write(json.dumps(response_data, indent=4).encode())
