e6656a856fa8539e57ddc708d5a22bf7b63f7612
Borsch <l.boristag@gmail.com>
2025-02-24 01:28:44 +0300

    config eternity added

---
+++ api/config/config_interface.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+import yaml
+
+@dataclass
+class Config:
+    HOST: str = 'localhost'
+    PORT: int = 8080
+
+    def get_address(self):
+        return self.HOST, self.PORT
+
+
+def parse_config(path='config.yaml') -> Config:
+    with open(path, 'r') as f:
+        config = yaml.safe_load(f)
+        config = Config(**config)
+    return config
