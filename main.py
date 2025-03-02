f667204624977b8ac2a44659b7d535c0760edd91
Borsch <l.boristag@gmail.com>
2025-03-03 02:36:30 +0300

    initialized db and server

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from api.server import run_server
+import uvicorn
+from fastapi import FastAPI
+
+from routes.users_router import router_users
+
+app = FastAPI()
+
+# app.include_router(user_router, prefix="/user")
+# app.include_router(task_router, prefix="/task")
+
+app.include_router(router_users)
+
 
 
 if __name__ == "__main__":
-    run_server()
+    uvicorn.run(
+        "main:app",
+        host='localhost',
+        port=8080,
+        reload=True
+    )
+
